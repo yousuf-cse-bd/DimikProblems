b310db1e5a6fb94703fc453b1c9fb830e2d32740
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-25 09:21:06 +0600

    Division Ability 2.0

---
+++ DimikPro_34.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file DimikPro_33.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Division ability
+ * @version 0.1
+ * @date 2022-06-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int testCase;
+    cout<<"Enter Test Case: ";
+    cin>>testCase;
+    while(testCase --){
+        unsigned int A, B,C;
+        cout<<"\tEnter A , B. & C  values respectively: ";
+        cin>>A>>B>>C;
+        for(unsigned int i = 1; i<=C; i++){
+            if(i % A == 0 && i % B == 0){
+                cout<<i<<"\n";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
