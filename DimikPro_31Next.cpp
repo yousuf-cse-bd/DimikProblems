992309c46f0e905c9b38c1d7c4ebf8a10439d33c
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-06-25 08:00:18 +0600

    Perfect numbers shortcut method

--- DimikPro_31Next.cpp
+++ DimikPro_31Next.cpp
@@ -1,7 +1,7 @@
 /**
  * @file DimikPro_31Next.cpp
  * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
- * @brief Prime numbers
+ * @brief Perfect numbers
  * @version 0.1
  * @date 2022-06-24
  * 
@@ -16,13 +16,13 @@ int main(int argc, char const *argv[])
 {
     /* code */
     int count = 0;
-    unsigned int primeNumbers[5] = {6, 28, 496, 8128, 33550336};
+    unsigned int perfectNumbers[5] = {6, 28, 496, 8128, 33550336};
     int N;
     cout<<"1 to Nth prime number: ";
     cin>>N;
     for(short unsigned int i = 0; i<5; i++){
-        if(primeNumbers[i] <= N){
-            cout<<primeNumbers[i]<<endl;
+        if(perfectNumbers[i] <= N){
+            cout<<perfectNumbers[i]<<endl;
         }
         else{
             break;
