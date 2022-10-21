2c87ab76f9a3f443e1380a07fe86102b966eb5d6
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-21 22:29:08 +0600

    Summation of msd and lsd from a decimal number

--- DimikPro_6.cpp
+++ DimikPro_6.cpp
@@ -1,33 +1,33 @@
-///Dimik Problem 6
-///Author: Muhammad Yousuf Ali
-///Since: 12/12/19
+/**
+ * @file DimikPro_6.cpp
+ * @author your name (you@domain.com)
+ * @brief Summation of msd and lsd from a decimal number
+ * @version 0.1
+ * @date 2019-12-12
+ * @since ThursDay; 10:27 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int num, lsd,msd,div,sum,T;
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    int num, lsd, msd, sum, T;
     cin>>T;
-    while(T>0)
-    {
+    while(T--){
         sum = 0;
         cin>>num;
         lsd = num % 10;
-        while(num!=msd)
-        {
-            div = num / 10;
-            msd = div % 10;
-            num = div;
+        while(num != 0){
+            msd = num % 10;
+            num = num / 10;
         }
-
         sum = msd + lsd;
         cout<<"Sum = "<<sum<<endl;
-        sum = 0;
-
-        T--;
     }
-
     return 0;
 }
 
