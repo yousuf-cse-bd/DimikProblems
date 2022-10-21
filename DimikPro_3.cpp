97194cadb1c590fea56d45d815c9742264d55f36
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-21 20:45:08 +0600

    Descending order loop

--- DimikPro_3.cpp
+++ DimikPro_3.cpp
@@ -1,28 +1,28 @@
-/****
- * @Author: Muhammad Yousuf Ali
- * @Since: TuesDay; 17 September 2019
+/**
+ * @file DimikPro_3.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Descending order loop
+ * @version 0.1
+ * @date 2019-09-17
+ * @since TuesDay; 08:35
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
  */
 
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-
-    int i;
-    int count = 0;
-    for(i = 1000;i>=1;i--)
-    {
-         count ++;
-        if(count % 5 != 0)
-        {
-            cout<<i<<"\t";
+int main(int argc, char const *argv[]){
+    system("cls");
+    unsigned short int count = 0;
+    for(int i = 1000;i>=1;i--){
+        count ++;
+        if(count % 5 != 0){
+            cout<<i<<" ";
         }
-        else
-        {
+        else{
             cout<<i<<endl;
         }
-        
     }
 
     return 0;
