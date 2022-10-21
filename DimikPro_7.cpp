8c2bfca2d833620d858166f5a21176b1acb4a248
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-21 22:40:16 +0600

    Count number from string

--- DimikPro_7.cpp
+++ DimikPro_7.cpp
@@ -1,33 +1,39 @@
-///Author: Muhammad Yousuf Ali
-///Since: 13/12/2019
-
+/**
+ * @file DimikPro_7.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Count number from string
+ * @version 0.1
+ * @date 2019-09-13
+ * @since FriDay; 10:27 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
-int main(void)
-{
+int main(int argc, char const *argv[]){
+    system("cls");
     char line[100000];
     int testCase, count;
     cout<<"Enter TestCase Value Here: ";
     cin>>testCase;
     cin.ignore();
-    while(testCase--)
-    {
+    while(testCase--){
         char *p, *e;
         cout<<"Enter Line: ";
         cin.getline(line, 100000);
         cout<<"Entered Line: "<<line<<endl;
         count = 0;
-        for (p = line; ; p = e)
-        {
-            strtol(p, &e, 10);
-            if(p==e)
-            break;
-            else
-            count++;
+        for (p = line; ; p = e){
+            strtol(p, &e, 10); /*string to long*/
+            if(p==e){
+                break;
+            }
+            else{
+                count++;
+            }
         }
-
         cout<<count<<endl;
     }
 
