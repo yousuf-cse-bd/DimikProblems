c9d6a77ddd241c431e0cc4bfa75035faa23da019
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-21 21:44:46 +0600

    Find maximum divisor

--- DimikPro_4.cpp
+++ DimikPro_4.cpp
@@ -1,26 +1,30 @@
-/****
- * @Author: Muhammad Yousuf Ali
- * @Since: TuesDay; 17 September 2019
+/**
+ * @file DimikPro_4.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Find maximum divisor
+ * @version 0.1
+ * @date 2019-09-17
+ * @since: TuesDay; 10:00 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
  */
 
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    int  testCase,i,j,num;
+int main(int argc, char const *argv[]){
+    system("cls");
+    unsigned short int testCase;
+    unsigned int i,j,num;
     //cout<<"Enter TestCase: ";
     cin>>testCase;
 
     j = 1;
-    while(j<=testCase)
-    {
+    while(j <= testCase){
         cin>>num;
         cout<<"Case "<<j<<":";
-        for(i = 1 ;i<=num; i++)
-        {
-            if(num % i == 0)
-            {
+        for(i = 1 ;i<=num; i++){
+            if(num % i == 0){
                 cout<<" "<<i;
             }
         }
@@ -28,9 +32,5 @@ int main(int argc, char const *argv[])
         cout<<endl;
     }
     
-    
-
-
-
     return 0;
 }
