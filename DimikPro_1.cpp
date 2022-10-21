7b17ad3f3cca762913b4ab5765c4cd54061d53f3
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-21 17:05:17 +0600

    Simple odd even a positive integer

--- DimikPro_1.cpp
+++ DimikPro_1.cpp
@@ -1,29 +1,32 @@
-///Dimik Problem 1
-///Author: Muhammad Yousuf Ali
-///Since: 12/12/19
+/**
+ * @file DimikPro_1.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Simple odd even a positive integer
+ * @version 0.1
+ * @date 2019-12-12
+ * @since ThursDay; 10:00 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int t,num;
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    unsigned short int t;
+    unsigned int number;
     cin>>t;
-    while(t>0)
-    {
-        cin>>num;
-        if(num % 2 == 0)
-        {
+    while(t--){
+        cin>>number;
+        if(number % 2 == 0){
             cout<<"even"<<endl;
-        }
-        else
-        {
+        }else{
             cout<<"odd"<<endl;
         }
-        t--;
     }
-
-
     return 0;
 }
+
 
