85eebef45726e92e4c41108a74c7f7f6e8ebb51b
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-22 14:51:12 +0600

    Check a positive number is square or not

--- DimikPro_9.cpp
+++ DimikPro_9.cpp
@@ -1,27 +1,32 @@
+/**
+ * @file DimikPro_9.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Check a positive number is square or not
+ * @version 0.1
+ * @date 2022-10-22
+ * @since SaturDay; 02:50 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
-#include <math.h>
+#include <cmath>
 using namespace std;
 
-int main(void)
-{
-
-    int number,t, tmp;
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    unsigned short int t;
+    unsigned int number, tmp;
     cin>>t;
-    while(t>0)
-    {
+    while(t--){
         cin>>number;
         tmp = sqrt(number);
-        if(number == pow(tmp,2))
-        {
+        if(number == pow(tmp,2)){
             cout<<"YES"<<endl;
         }
-        else
-        {
+        else{
             cout<<"NO"<<endl;
         }
-
-        t -- ;
     }
-
     return 0;
 }
