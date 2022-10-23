bae5557785884ea98536a198f4f1fb4b3c55d4f1
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-23 16:38:15 +0600

    Generate factorial value max 15

--- DimikPro_11.cpp
+++ DimikPro_11.cpp
@@ -1,21 +1,29 @@
+/**
+ * @file DimikPro_11.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Generate factorial value max 15
+ * @version 0.1
+ * @date 2022-10-23
+ * @since 04:37 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int t,i,n;
-    long long int fact = 1;
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    unsigned short int t, n;
+    unsigned long long int fact;
     cin>>t;
-    while(t--)
-    {
+    while(t--){
         cin>>n;
-        for(i = 2 ;i<=n;i++)
-        {
+        fact = 1;
+        for(unsigned short int i = 2 ; i <=n ; i++){
             fact = fact * i;
         }
         cout<<fact<<endl;
-        fact = 1;
     }
-
     return 0;
 }
